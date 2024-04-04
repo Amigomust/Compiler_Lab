4671bd12811d6e1fe5cbb7bdeff9c02e02cbb517
Amigomust <albertamigo@foxmail.com>
2024-04-04 19:11:56 +0800

    Refactor code to improve performance and readability

--- yue.cpp
+++ yue.cpp
@@ -14,17 +14,18 @@
 #define TYPEMASK 0x00ff0000
 #define VALMASK 0xff000000
 
-using ui = unsigned int;
-
 using namespace std;
-int valnum = 0;
+
+using PSS = pair<string, string>;
 
 /*
 function: 2
 int: 1
 special: 0
+intargs: 3
 */
 
+// Class Definition
 class ElementID {
     private:
         int id;
@@ -60,9 +61,23 @@ class Trie {
             }
             nodes[beg].value.push(v);
         }
+        void Remove(string& s) {
+            int beg = 0;
+            for (int i = 0; i < s.size(); i ++) {
+                unordered_map<char, int>& next = nodes[beg].next;
+                unordered_map<char, int>::iterator it = next.find(s[i]);
+                if (it == next.end()) return;
+                beg = it->second;
+            }
+            if (!nodes[beg].value.empty())
+                nodes[beg].value.pop();
+            return;
+        }
+
         Trie() {
             nodes.push_back(Node());
         }
+
         ElementID Find(string& s, int valid) {
             int beg = 0;
             for (int i = 0; i < s.size(); i ++) {
@@ -79,40 +94,29 @@ class Trie {
 class Element {
     protected:
         int stackPos;
-        size_t needMemory;
     public:
-        Element(int pos = 0, size_t need = 0) {
+        Element(int pos = 0) {
             stackPos = pos;
-            needMemory = need;
         }
 };
 
 // flag = 1: immediate value, flag = 0: stack value
 class IntElement: Element{
     private:
+        string name;
         int value;
         int flag;
     public:
-        IntElement(int v, int stackPos, int f = 0) {
+        IntElement(string n, int v, int stackPos, int f = 0) {
+            name = n;
             value = v;
             stackPos = stackPos;
             f = flag;
-            needMemory = 4;
-            // TODO: complete the mips code
         }
-        IntElement(IntElement& a) {
-            value = a.value;
-            stackPos = a.stackPos;
-            flag = a.flag;
-            needMemory = 4;
-            // TODO: complete the mips code
-        }
-        ~IntElement() {
-            // TODO: complete the mips code
-        }
+        static size_t getNeedMemory() { return 4; }
         friend void Add(const IntElement& a, const IntElement& b, IntElement& c) {
             // TODO: complete the mips code
-            if (a.flag && b.flag) c = IntElement(a.value + b.value, 0, 1);
+            if (a.flag && b.flag) c = IntElement(c.name, a.value + b.value, 0, 1);
             else if (a.flag) {
                 // c = b + immediate value
             } else if (b.flag) {
@@ -121,72 +125,82 @@ class IntElement: Element{
                 // c = a + b
             }
         }
-        IntElement operator-(const IntElement& a) const {
+        friend IntElement Sub(const IntElement& a, const IntElement& b, IntElement& c) {
             // TODO: complete the mips code
-            return IntElement(value - a.value, stackPos);
+            return IntElement(c.name, 0, c.stackPos);
         }
-        IntElement operator*(const IntElement& a) const {
+        friend IntElement Mul(const IntElement& a, const IntElement& b, IntElement& c) {
             // TODO: complete the mips code
-            return IntElement(value * a.value, stackPos);
+            return IntElement(c.name, a.value * b.value, c.stackPos);
         }
-        IntElement operator/(const IntElement& a) const {
+        friend IntElement Div(const IntElement& a, const IntElement& b, IntElement& c) {
             // TODO: complete the mips code
-            return IntElement(value / a.value, stackPos);
+            return IntElement(c.name, 0, c.stackPos);
         }
-        IntElement operator%(const IntElement& a) const {
+        friend IntElement Mod(const IntElement& a, const IntElement& b, IntElement& c) {
             // TODO: complete the mips code
-            return IntElement(value % a.value, stackPos);
+            return IntElement(c.name, 0, c.stackPos);
         }
-        IntElement operator<(const IntElement& a) const {
+        friend IntElement Less(const IntElement& a, const IntElement& b, IntElement& c) {
             // TODO: complete the mips code
-            return IntElement(value < a.value, stackPos);
+            return IntElement(c.name, 0, c.stackPos);
         }
-        IntElement operator<=(const IntElement& a) const {
+        friend IntElement LessEqual(const IntElement& a, const IntElement& b, IntElement& c) {
             // TODO: complete the mips code
-            return IntElement(value <= a.value, stackPos);
+            return IntElement(c.name, 0, c.stackPos);
         }
-        IntElement operator>(const IntElement& a) const {
+        friend IntElement Greater(const IntElement& a, const IntElement& b, IntElement& c) {
             // TODO: complete the mips code
-            return IntElement(value > a.value, stackPos);
+            return IntElement(c.name, 0, c.stackPos);
         }
-        IntElement operator>=(const IntElement& a) const {
+        friend IntElement GreaterEqual(const IntElement& a, const IntElement& b, IntElement& c) {
             // TODO: complete the mips code
-            return IntElement(value >= a.value, stackPos);
+            return IntElement(c.name, 0, c.stackPos);
         }
-        IntElement operator==(const IntElement& a) const {
+        friend IntElement Equal(const IntElement& a, const IntElement& b, IntElement& c) {
             // TODO: complete the mips code
-            return IntElement(value == a.value, stackPos);
+            return IntElement(c.name, 0, c.stackPos);
         }
-        IntElement operator!=(const IntElement& a) const {
+        friend IntElement NotEqual(const IntElement& a, const IntElement& b, IntElement& c) {
             // TODO: complete the mips code
-            return IntElement(value != a.value, stackPos);
+            return IntElement(c.name, 0, c.stackPos);
         }
-        IntElement operator&(const IntElement& a) const {
+        friend IntElement And(const IntElement& a, const IntElement& b, IntElement& c) {
             // TODO: complete the mips code
-            return IntElement(value & a.value, stackPos);
+            return IntElement(c.name, 0, c.stackPos);
         }
-        IntElement operator|(const IntElement& a) const {
+        friend IntElement Or(const IntElement& a, const IntElement& b, IntElement& c) {
             // TODO: complete the mips code
-            return IntElement(value | a.value, stackPos);
+            return IntElement(c.name, 0, c.stackPos);
         }
-        IntElement operator^(const IntElement& a) const {
+        friend IntElement Xor(const IntElement& a, const IntElement& b, IntElement& c) {
             // TODO: complete the mips code
-            return IntElement(value ^ a.value, stackPos);
+            return IntElement(c.name, 0, c.stackPos);
+        }
+        friend void Assign(const IntElement a, const IntElement b) {
+            return;
+        }
+};
+
+class ArgIntElement: IntElement {
+    private:
+        int argidx;
+    public:
+        ArgIntElement(int idx, string n, int v = 0, int stackPos = 0, int f = 1): IntElement(n, v, stackPos, f){
+            argidx = idx;
         }
 };
 
 class FunctionElement {
     private:
         string name;
-        vector<string> args;
-        vector<string> types;
+        vector<PSS> args;
         string codes;
         string return_type;
     public:
-        FunctionElement(string n, vector<string> a, vector<string> v, string c, string rt) {
+        FunctionElement(string n, vector<PSS> a, string c, string rt) {
             name = n;
             args = a;
-            types = v;
             codes = c;
             return_type = rt;
         }
@@ -196,18 +210,50 @@ class FunctionElement {
         }
 };
 
+stack<string> VariableName;
+int id = 0;
+int valid = 0;
+int globalStackPos = 0;
+const IntElement zero = {"zero", 0, 0, 1};
+
+// Function 
 // TODO: complete the mips code
-template<typename T>
-void allocStack();
 
-template<typename T>
-void freeStack();
+template<class T>
+void allocStack() {
+    printf("addiu $sp, $sp, -%d\n", T.getNeedMemory());
+    globalStackPos += T.getNeedMemory();
+}
 
+template<class T>
+void freeStack() {
+    printf("addiu $sp, $sp, %d\n", T.getNeedMemory());
+    globalStackPos -= T.getNeedMemory();
+}
+
+string RemoveBegSpaceAndEndSpace(string s);
+
+void end();
+
+void CompileTheCode(string s);
+
+void CompileTheExpression(string s);
+
+string Regular(string s);
+
+FunctionElement CreateFunctionElement(string name, vector<string>& arglist, string codes, string rt = "int");
+
+void CreateAssignElement(string s);
+
+IntElement CreateIntElement(string name, int flag = 0, int value = 0, int needToInsert = 1);
+
+ArgIntElement CreateArgIntElement(int idx, string name, int value = 0);
+
+void RemoveFunctionElement();
 /*
 表达式求值
 */
-template<typename T>
-T calExpre(string& s) {
+void CreateAssignElement(string s) {
     unordered_map<string, int> Sin = { 
         {"(", 1}, {")", 12}, 
         {"*", 11}, {"%", 11}, {"/", 11}, 
@@ -223,16 +269,16 @@ T calExpre(string& s) {
          {"&", 4}, {"^", 4}, {"|", 4},
          {"=", 2}};
     
-    stack<T> num; stack<string> op;
+    stack<IntElement> num; stack<string> op;
     int n = s.size();
 
-    auto cal = [&num, &op, &tempValNum]() {
+    auto cal = [&num, &op]() {
         string c = op.top(); op.pop();
         if (c == "(") return;
-        T a = num.top(); num.pop();
-        T b = num.top(); num.pop();
-        T temp;
-        tempValNum ++;
+        IntElement a = num.top(); num.pop();
+        IntElement b = num.top(); num.pop();
+        IntElement temp = CreateIntElement("", 0, 0, 0);
+        Assign(temp, zero);
         if (c == "+") temp = a + b;
         else if (c == "-") temp = a - b;
         else if (c == "*") temp = a * b;
@@ -256,30 +302,28 @@ T calExpre(string& s) {
     "<", ">", "<=", ">=", "==", 
     "&", "|", "^"};
 
-	for (int i = 0; i < n; i++)
-		if ('0' <= s[i] && s[i] <= '9') {
-			int k = i + 1;
-			int res = s[i] - '0';
-			while (k < n && '0' <= s[k] && s[k] <= '9')
-				res = res * 10 + (s[k ++] - '0');
-			num.push(res);
-			i = k - 1;
-		} 
-		else if (s[i] == ')') {
-			while (op.size() && op.top() != '(') cal();
-			op.pop();
-		}
-		else {
-			while (op.size() && Sin[op.top()] > Sout[s[i]]) cal();
-			op.push(s[i]);
-		}
+	// for (int i = 0; i < n; i++)
+	// 	if ('0' <= s[i] && s[i] <= '9') {
+	// 		int k = i + 1;
+	// 		int res = s[i] - '0';
+	// 		while (k < n && '0' <= s[k] && s[k] <= '9')
+	// 			res = res * 10 + (s[k ++] - '0');
+	// 		num.push(res);
+	// 		i = k - 1;
+	// 	} 
+	// 	else if (s[i] == ')') {
+	// 		while (op.size() && op.top() != '(') cal();
+	// 		op.pop();
+	// 	}
+	// 	else {
+	// 		while (op.size() && Sin[op.top()] > Sout[s[i]]) cal();
+	// 		op.push(s[i]);
+	// 	}
     
 	while (op.size()) cal();
-
-	return num.top();
 }
 
-string rmBegSpaceAndEndSpace(string s) {
+string RemoveBegSpaceAndEndSpace(string s) {
     int len = s.size();
     int beg = 0;
     while (beg < len && s[beg] == ' ') beg ++;
@@ -288,28 +332,67 @@ string rmBegSpaceAndEndSpace(string s) {
     return s.substr(beg, end - beg + 1);
 }
 
-FunctionElement createFunctionElement(string name, vector<string>& arglist, string codes, string rt = "int") {
-    vector<string> args;
-    vector<string> types;
+void RemoveFunctionElement() {
+    while (1) {
+        ElementID Eid = GlobalTrie.Find(VariableName.top(), valid);
+        if (Eid.getValidSpace() == valid) {
+            GlobalTrie.Remove(VariableName.top());
+            VariableName.pop();
+            id --;
+        } else break;
+    }
+    valid --;
+}
+
+FunctionElement CreateFunctionElement(string name, vector<string>& arglist, string codes, string rt = "int") {
+    printf("%s:\n", name.c_str());
+    vector<PSS> args;
     for (int i = 0; i < arglist.size(); i ++) {
         string arg = arglist[i];
         assert(arg.size() != 0);
         int pos = arg.find(' ');
-        string type = rmBegSpaceAndEndSpace(arg.substr(0, pos));
-        string temp = rmBegSpaceAndEndSpace(arg.substr(pos + 1));
-
-        args.push_back(name);
-        types.push_back(type);
+        string type = RemoveBegSpaceAndEndSpace(arg.substr(0, pos));
+        string name = RemoveBegSpaceAndEndSpace(arg.substr(pos + 1));
+        args.emplace_back(type, name);
     }
-    return FunctionElement(name, args, types, codes, rt);
+    ElementID functionId = ElementID(id ++, 2, valid);
+    GlobalTrie.Insert(name, functionId);
+    valid ++;
+    for (int i = 0; i < args.size(); i ++) {
+        auto [type, name] = args[i];
+        if (type == "int") {
+            CreateArgIntElement(i, name, 0);
+        }
+        else assert(0); // Compile Error if the type is not Exist
+    }
+    valid ++;
+    CompileTheCode(codes);
+    RemoveFunctionElement(); // Remove the function elements
+    RemoveFunctionElement(); // Remove the arg elements
+    return FunctionElement(name, args, codes, rt);
 }
 
-void CompileTheCode(string s = "int main(int a, int b, int c) {};") {
-    // TODO: Compile the code
-    beg();
-
-    end();
+/*
+name: variable name
+valid: valid space
+id: varialble id
+flag: 1: immediate value, 0: stack value
+value: immediate value
+needToInsert: 1: need to insert into the trie, 0: no need to insert into the trie
+*/
+IntElement CreateIntElement(string name, int flag = 0, int value = 0, int needToInsert = 1) {
+    if (flag) {
+        return {name, value, globalStackPos, 1};
+    } else {
+        if (needToInsert) {
+            ElementID Eid = {id ++, 1, valid};
+            GlobalTrie.Insert(name, Eid);
+        }
+        allocStack<IntElement>();
+        return {name, value, globalStackPos, 0};
+    }
 }
+
 
 void end() {
     printf("move $a0, $v0\n");
@@ -327,7 +410,7 @@ void beg() {
     printf("addiu $sp, $sp, -0x100\n");
 }
 
-string Regular(string& s) {
+string Regular(string s) {
     int len = s.size();
     string res;
     for (int i = 0; i < len; i ++) {
@@ -338,25 +421,82 @@ string Regular(string& s) {
             while (k < len && s[k] == ' ') k++;
             i = k - 1;
             res += ' ';
-        } else if (s[i] == '}' && i + 1 < len && s[i + 1] != ';') res += "};";
+        } else if (s[i] == '}' && (i + 1 == len || s[i + 1] != ';')) res += "};";
         else res += s[i];
     }
+    debug(res);
     assert(res.back() == ';'); // Compile Error if the last char is not ';'
     return res;
 }
 
+void CompileTheCode(string s) {
+    // beg();
+    s = RemoveBegSpaceAndEndSpace(s);
+    if (s.size() == 0) return;
+    vector<int> Brackets(3);
+    unordered_map<char, int> BracketsMap = {{'{', 0}, {'(', 1}, {'[', 2}, {'}', 0}, {')', 1}, {']', 2}};
+    for (int i = 0; i < s.size(); i ++) {
+        int l = i;
+        while (s[i] != ';' || (Brackets[0] || Brackets[1] || Brackets[2])) {
+            if (s[i] == '{' || s[i] == '(' || s[i] == '[') Brackets[BracketsMap[s[i]]] ++;
+            else if (s[i] == '}' || s[i] == ')' || s[i] == ']') Brackets[BracketsMap[s[i]]] --;
+            assert(Brackets[0] >= 0 && Brackets[1] >= 0 && Brackets[2] >= 0); // Compile Error if the brackets are not matched
+            i ++;
+        }
+        
+        if (i != l) {
+            debug(s.substr(l, i - l));
+            CompileTheExpression(s.substr(l, i - l));
+        }
+    }
+    // end();
+}
+
+void CompileTheExpression(string s) {
+    regex FunctionExpression("(int|void)\\s*([a-z_A-Z][a-z_A-Z0-9]*)\\s*\\((.*)\\)\\s*\\{(.*)\\}");
+    regex DefineExpression("(int)\\s*([a-z_A-Z][a-z_A-Z0-9]*)");
+    regex AssignExpression("([a-z_A-Z][a-z_A-Z0-9]*)\\s*=\\s*(.*)");
+    smatch sm;
+    if (regex_match(s, sm, FunctionExpression)) { // Compile the Function
+        string type = sm[1];
+        string name = sm[2];
+        string args = sm[3];
+        vector<string> arglist;
+        args = RemoveBegSpaceAndEndSpace(args);
+        int argslen = args.size();
+        for (int i = 0; i < argslen; i ++) {
+            int l = i;
+            while (i < argslen && args[i] != ',') i ++;
+            assert(i != l);
+            string arg = RemoveBegSpaceAndEndSpace(args.substr(l, i - l));
+            if (arg.size()) arglist.push_back(arg);
+        }
+        string codes = sm[4];
+        CreateFunctionElement(name, arglist, codes, type);
+    } else if (regex_match(s, DefineExpression)) {
+        string type = sm[1];
+        string name = sm[2];
+        if (type == "int") CreateIntElement(name);
+        else assert(0); // Compile Error if the type is not Exist
+    } else if (regex_match(s, AssignExpression)) {
+        CreateAssignElement(s);
+    }
+        
+}
 
 int main(int argc, char* argv[]) {
-    assert(argc > 1);
-    fstream fs;
-    fs.open(argv[1], ios::in);
-    assert(fs.is_open());
-    string code;
-    string temp;
-    while (getline(fs, temp)) {
-        code += temp;
-    }
-    fs.close();
-    CompileTheCode(Regular(code));
-    return 0;
+    // assert(argc > 1);
+    // fstream fs;
+    // fs.open(argv[1], ios::in);
+    // assert(fs.is_open());
+    // string code;
+    // string temp;
+    // while (getline(fs, temp)) {
+    //     code += temp;
+    // }
+    // fs.close();
+    // CompileTheCode(Regular(code), 0);
+    // return 0;
+    CompileTheCode(Regular("int main() {int a = 1; int b = 2; int c = a + b;}"));
+    RemoveFunctionElement();
 }
