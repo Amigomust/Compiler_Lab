99357f1054cc31044f80295d04e1afd1b2cab2f7
xiao-sun-sun <13381371495@163.com>
2024-04-10 16:04:16 +0800

    hello

--- IntObj.cpp
+++ IntObj.cpp
@@ -19,150 +19,264 @@ Object* IntObject::add(Object* obj) {
     std::string aPos = toReg();
     if (intObj) {
         std::string bPos = intObj->toReg();
-        
+        printf("add %s, %s, %s\n", aPos.c_str(), aPos.c_str(), bPos.c_str());
         intObj->freeReg(0);
         return this;
     }
     ImmObject* immObj = dynamic_cast<ImmObject*>(obj);
     if (immObj) {
-        return nullptr;
+        std::string bPos = immObj->toReg();
+        printf("add %s, %s, %s\n", aPos.c_str(), aPos.c_str(), bPos.c_str());
+        immObj->freeReg(0);
+        return this;
     }
 }
 
 Object* IntObject::sub(Object* obj) {
     IntObject* intObj = dynamic_cast<IntObject*>(obj);
-    if (intObj) {
-        return nullptr;
+    std::string aPos = toReg();
+     if (intObj) {
+        std::string bPos = intObj->toReg();
+        printf("sub %s, %s, %s\n", aPos.c_str(), aPos.c_str(), bPos.c_str());
+        intObj->freeReg(0);
+        return this;
     }
     ImmObject* immObj = dynamic_cast<ImmObject*>(obj);
     if (immObj) {
-        return nullptr;
+        std::string bPos = immObj->toReg();
+        printf("sub %s, %s, %s\n", aPos.c_str(), aPos.c_str(), bPos.c_str());
+        immObj->freeReg(0);
+        return this;
     }
 }
 
 Object* IntObject::div(Object* obj) {
     IntObject* intObj = dynamic_cast<IntObject*>(obj);
-    if (intObj) {
-        return nullptr;
+    std::string aPos = toReg();
+     if (intObj) {
+        std::string bPos = intObj->toReg();
+        printf("div %s, %s\n", aPos.c_str(), bPos.c_str());
+        printf("mflo %s\n", aPos.c_str());
+        intObj->freeReg(0);
+        return this;
     }
     ImmObject* immObj = dynamic_cast<ImmObject*>(obj);
     if (immObj) {
-        return nullptr;
+        std::string bPos = immObj->toReg();
+        printf("div %s, %s\n", aPos.c_str(), bPos.c_str());
+        printf("mflo %s\n", aPos.c_str());
+        immObj->freeReg(0);
+        return this;
     }
 }
 
 Object* IntObject::mul(Object* obj) {
     IntObject* intObj = dynamic_cast<IntObject*>(obj);
-    if (intObj) {
-        return nullptr;
+    std::string aPos = toReg();
+     if (intObj) {
+        std::string bPos = intObj->toReg();
+        printf("mul %s, %s, %s\n", aPos.c_str(), aPos.c_str(), bPos.c_str());
+        intObj->freeReg(0);
+        return this;
     }
     ImmObject* immObj = dynamic_cast<ImmObject*>(obj);
     if (immObj) {
-        return nullptr;
+        std::string bPos = immObj->toReg();
+        printf("mul %s, %s, %s\n", aPos.c_str(), aPos.c_str(), bPos.c_str());
+        immObj->freeReg(0);
+        return this;
     }
 }
 
 Object* IntObject::mod(Object* obj) {
     IntObject* intObj = dynamic_cast<IntObject*>(obj);
-    if (intObj) {
-        return nullptr;
+    std::string aPos = toReg();
+     if (intObj) {
+        std::string bPos = intObj->toReg();
+        printf("div %s, %s\n", aPos.c_str(), bPos.c_str());
+        printf("mfhi %s\n", aPos.c_str());
+        intObj->freeReg(0);
+        return this;
     }
     ImmObject* immObj = dynamic_cast<ImmObject*>(obj);
     if (immObj) {
-        return nullptr;
+        std::string bPos = immObj->toReg();
+        printf("div %s, %s\n", aPos.c_str(), bPos.c_str());
+        printf("mfhi %s\n", aPos.c_str());
+        immObj->freeReg(0);
+        return this;
     }
 }
 
 Object* IntObject::and_(Object* obj) {
     IntObject* intObj = dynamic_cast<IntObject*>(obj);
+    std::string aPos = toReg();
     if (intObj) {
-        return nullptr;
+        std::string bPos = intObj->toReg();
+        printf("and %s, %s, %s\n", aPos.c_str(), aPos.c_str(), bPos.c_str());
+        intObj->freeReg(0);
+        return this;
     }
     ImmObject* immObj = dynamic_cast<ImmObject*>(obj);
     if (immObj) {
-        return nullptr;
+        std::string bPos = immObj->toReg();
+        printf("and %s, %s, %s\n", aPos.c_str(), aPos.c_str(), bPos.c_str());
+        immObj->freeReg(0);
+        return this;
     }
 }
 
 Object* IntObject::or_(Object* obj) {
     IntObject* intObj = dynamic_cast<IntObject*>(obj);
+    std::string aPos = toReg();
     if (intObj) {
-        return nullptr;
+        std::string bPos = intObj->toReg();
+        printf("or %s, %s, %s\n", aPos.c_str(), aPos.c_str(), bPos.c_str());
+        intObj->freeReg(0);
+        return this;
     }
     ImmObject* immObj = dynamic_cast<ImmObject*>(obj);
     if (immObj) {
-        return nullptr;
+        std::string bPos = immObj->toReg();
+        printf("or %s, %s, %s\n", aPos.c_str(), aPos.c_str(), bPos.c_str());
+        immObj->freeReg(0);
+        return this;
     }
 }
 
 Object* IntObject::xor_(Object* obj) {
     IntObject* intObj = dynamic_cast<IntObject*>(obj);
+    std::string aPos = toReg();
     if (intObj) {
-        return nullptr;
+        std::string bPos = intObj->toReg();
+        printf("xor %s, %s, %s\n", aPos.c_str(), aPos.c_str(), bPos.c_str());
+        intObj->freeReg(0);
+        return this;
     }
     ImmObject* immObj = dynamic_cast<ImmObject*>(obj);
     if (immObj) {
-        return nullptr;
+        std::string bPos = immObj->toReg();
+        printf("xor %s, %s, %s\n", aPos.c_str(), aPos.c_str(), bPos.c_str());
+        immObj->freeReg(0);
+        return this;
     }
 }
 
 Object* IntObject::lessEqual(Object* obj) {
     IntObject* intObj = dynamic_cast<IntObject*>(obj);
+    std::string aPos = toReg();
     if (intObj) {
-        return nullptr;
+        std::string bPos = intObj->toReg();
+        printf("slt %s, %s, %s\n", aPos.c_str(), bPos.c_str(), aPos.c_str());
+        printf("xori %s, %s, 1\n", aPos.c_str());
+        intObj->freeReg(0);
+        return this;
     }
     ImmObject* immObj = dynamic_cast<ImmObject*>(obj);
     if (immObj) {
-        return nullptr;
+        std::string bPos = immObj->toReg();
+        printf("slt %s, %s, %s\n", aPos.c_str(), bPos.c_str(), aPos.c_str());
+        printf("xori %s, %s, 1\n", aPos.c_str());
+        immObj->freeReg(0);
+        return this;
     }
 }
 Object* IntObject::lessThan(Object* obj) {
     IntObject* intObj = dynamic_cast<IntObject*>(obj);
+    std::string aPos = toReg();
     if (intObj) {
-        return nullptr;
+        std::string bPos = intObj->toReg();
+        printf("slt %s, %s, %s\n", aPos.c_str(), aPos.c_str(), bPos.c_str());
+        intObj->freeReg(0);
+        return this;
     }
     ImmObject* immObj = dynamic_cast<ImmObject*>(obj);
     if (immObj) {
-        return nullptr;
+        std::string bPos = immObj->toReg();
+        printf("slt %s, %s, %s\n", aPos.c_str(), aPos.c_str(), bPos.c_str());
+        immObj->freeReg(0);
+        return this;
     }
 }
 Object* IntObject::greaterEqual(Object* obj) {
     IntObject* intObj = dynamic_cast<IntObject*>(obj);
+    std::string aPos = toReg();
     if (intObj) {
-        return nullptr;
+        std::string bPos = intObj->toReg();
+        printf("slt %s, %s, %s\n", aPos.c_str(), aPos.c_str(), bPos.c_str());
+        printf("xori %s, %s, 1\n", aPos.c_str());
+        intObj->freeReg(0);
+        return this;
     }
     ImmObject* immObj = dynamic_cast<ImmObject*>(obj);
     if (immObj) {
-        return nullptr;
+        std::string bPos = immObj->toReg();
+        printf("slt %s, %s, %s\n", aPos.c_str(), aPos.c_str(), bPos.c_str());
+        printf("xori %s, %s, 1\n", aPos.c_str());
+        immObj->freeReg(0);
+        return this;
     }
 }
 Object* IntObject::greaterThan(Object* obj) {
     IntObject* intObj = dynamic_cast<IntObject*>(obj);
+    std::string aPos = toReg();
     if (intObj) {
-        return nullptr;
+        std::string bPos = intObj->toReg();
+        printf("slt %s, %s, %s\n", aPos.c_str(), bPos.c_str(), aPos.c_str());
+        intObj->freeReg(0);
+        return this;
     }
     ImmObject* immObj = dynamic_cast<ImmObject*>(obj);
     if (immObj) {
-        return nullptr;
+        std::string bPos = immObj->toReg();
+        printf("slt %s, %s, %s\n", aPos.c_str(), bPos.c_str(), aPos.c_str());
+        immObj->freeReg(0);
+        return this;
     }
 }
 Object* IntObject::notEqual(Object* obj) {
     IntObject* intObj = dynamic_cast<IntObject*>(obj);
+    std::string aPos = toReg();
     if (intObj) {
-        return nullptr;
+        std::string bPos = intObj->toReg();
+        printf("beq %s, %s, 2\n", aPos.c_str(), bPos.c_str());
+        printf("li %s, 0\n", aPos.c_str());
+        printf("addi %s, 1\n", aPos.c_str());
+        printf("li %s, 0\n", aPos.c_str());
+        intObj->freeReg(0);
+        return this;
     }
     ImmObject* immObj = dynamic_cast<ImmObject*>(obj);
     if (immObj) {
-        return nullptr;
+        std::string bPos = immObj->toReg();
+        printf("beq %s, %s, 2\n", aPos.c_str(), bPos.c_str());
+        printf("li %s, 0\n", aPos.c_str());
+        printf("addi %s, 1\n", aPos.c_str());
+        printf("li %s, 0\n", aPos.c_str());
+        immObj->freeReg(0);
+        return this;
     }
 }
 Object* IntObject::equal(Object* obj) {
     IntObject* intObj = dynamic_cast<IntObject*>(obj);
+    std::string aPos = toReg();
     if (intObj) {
-        return nullptr;
+        std::string bPos = intObj->toReg();
+        printf("bne %s, %s, 2\n", aPos.c_str(), bPos.c_str());
+        printf("li %s, 0\n", aPos.c_str());
+        printf("addi %s, 1\n", aPos.c_str());
+        printf("li %s, 0\n", aPos.c_str());
+        intObj->freeReg(0);
+        return this;
     }
     ImmObject* immObj = dynamic_cast<ImmObject*>(obj);
     if (immObj) {
-        return nullptr;
+        std::string bPos = immObj->toReg();
+        printf("bne %s, %s, 2\n", aPos.c_str(), bPos.c_str());
+        printf("li %s, 0\n", aPos.c_str());
+        printf("addi %s, 1\n", aPos.c_str());
+        printf("li %s, 0\n", aPos.c_str());
+        immObj->freeReg(0);
+        return this;
     }
 }
